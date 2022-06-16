f03013be53c5674348b7e2b3d61f749e14703e16
Jose Arsuaga <jose.arsuaga@walmart.com>
2022-06-16 11:40:18 -0500

    Initial Commit

---
+++ MVVM and Combine/HomeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  HomeViewController.swift
+//  MVVM and Combine
+//
+//  Created by Jose Arsuaga on 16/06/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet private weak var welcomeLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
